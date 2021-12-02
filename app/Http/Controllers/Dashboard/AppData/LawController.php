bc34eda6224453e146f48e35d52749ec33020193
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2021-12-02 13:26:20 +0200

    .

--- app/Http/Controllers/Dashboard/AppData/LawController.php
+++ app/Http/Controllers/Dashboard/AppData/LawController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard\AppData;
 
 use App\Http\Controllers\Dashboard\Controller;
+use App\Models\Category;
 use App\Models\Law;
 use App\Traits\AhmedPanelTrait;
 
@@ -29,6 +30,22 @@ class LawController extends Controller
                 'is_searchable'=>true,
                 'order'=>true
             ],
+            'category_id'=> [
+                'name'=>'category_id',
+                'type'=>'custom_relation',
+                'relation'=>[
+                    'data'=> Category::all(),
+                    'custom_search'=>function($Object){
+                        return ($Object)?app()->getLocale() == 'ar'?$Object->name_ar:$Object->name:'-';
+                    },
+                    'custom'=>function($Object){
+                        return ($Object)?app()->getLocale() == 'ar'?$Object->name_ar:$Object->name:'-';
+                    },
+                    'entity'=>'category'
+                ],
+                'is_searchable'=>true,
+                'order'=>true
+            ],
         ]);
         $this->setFields([
             'name'=> [
@@ -39,6 +56,18 @@ class LawController extends Controller
             'name_ar'=> [
                 'name'=>'name_ar',
                 'type'=>'text',
+                'is_required'=>true
+            ],
+            'category_id'=> [
+                'name'=>'category_id',
+                'type'=>'custom_relation',
+                'relation'=>[
+                    'data'=> Category::all(),
+                    'custom'=>function($Object){
+                        return app()->getLocale() == 'ar'?$Object->name_ar:$Object->name;
+                    },
+                    'entity'=>'category'
+                ],
                 'is_required'=>true
             ],
         ]);
--- app/Models/Law.php
+++ app/Models/Law.php
@@ -3,16 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @property mixed id
  * @property mixed name
  * @property mixed name_ar
+ * @property mixed category_id
  */
 class Law extends Model
 {
     protected $table = 'laws';
-    protected $fillable = ['name','name_ar'];
+    protected $fillable = ['name','name_ar','category_id'];
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
     /**
      * @return mixed
      */
@@ -59,6 +65,22 @@ class Law extends Model
     public function setNameAr($name_ar): void
     {
         $this->name_ar = $name_ar;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategoryId()
+    {
+        return $this->category_id;
+    }
+
+    /**
+     * @param mixed $category_id
+     */
+    public function setCategoryId($category_id): void
+    {
+        $this->category_id = $category_id;
     }
 
 }
